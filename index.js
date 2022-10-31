6d32e32e09f3bdcc501f55fb1a84f15210ad63ce
Thon Ly <thonly@heartbank.ceo>
2022-10-30 19:54:49 -0700

    begin v1

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+import "./components/sw-header/element.mjs";
 
-  gtag('config', 'G-ZGWZ1ZWQKX');
+import "./components/sw-main/sw-curriculum/element.mjs";
+import "./components/sw-main/sw-stealth/element.mjs";
+import "./components/sw-main/sw-podcast/element.mjs";
+import "./components/sw-main/sw-quiz/element.mjs";
+import "./components/sw-main/sw-code/element.mjs";
+import "./components/sw-main/sw-cohort/element.mjs";
+import "./components/sw-main/sw-coupon/element.mjs";
+import "./components/sw-main/element.mjs";
+
+import "./components/sw-footer/element.mjs";
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-ZGWZ1ZWQKX');
