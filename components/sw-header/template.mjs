1e9262ae82d2ea9c9c4904126fc1ca1f48d736fa
Thon Ly <thonly@heartbank.ceo>
2022-11-10 14:53:01 -0800

    begin card

--- components/sw-header/template.mjs
+++ components/sw-header/template.mjs
@@ -6,97 +6,14 @@ template.innerHTML = `
     <link rel="stylesheet" href="components/sw-header/shadow.css">
     <header>
         <section>
-            <a href="#"><img src="siliconwat.png"></a>
+            <a href="https://siliconwat.com"><img src="siliconwat.png"></a>
             <a href="#"><h1>Frontend Music Course</h1></a>
         </section>
         <input id="menu-toggle" type="checkbox">
         <label class='menu-button-container' for="menu-toggle">
             <span class='menu-button'></span>
         </label>
-        <ul>
-            <!--<li>
-                <h3>Unit 1</h3>
-                <nav>
-                    <h2>HTML/CSS/JS Orchestra</h2>
-                    <sw-bar></sw-bar>
-                    <h4>Chapter 1: Hello World</h4>
-                    <menu>
-                        <li class="learn">
-                            <input id="learn-chapter1" type="checkbox" oninput="checkMark(this)">
-                            <a href="#learn-chapter1">Learn</a>
-                        </li>
-                        <li class="practice">
-                            <input id="practice-chapter1" type="checkbox" oninput="checkMark(this)">
-                            <a href="#practice-chapter1">Practice</a>
-                        </li>
-                        <li class="review">
-                            <input id="review-chapter1" type="checkbox" oninput="checkMark(this)">
-                            <a href="#review-chapter1">Review</a>
-                        </li>
-                    </menu>
-                    <h4>Chapter 2: JavaScript Console</h4>
-                    <menu>
-                        <li class="learn">
-                            <input id="learn-chapter2" type="checkbox" oninput="checkMark(this)">
-                            <a href="#learn-chapter2">Learn</a>
-                        </li>
-                        <li class="practice">
-                            <input id="practice-chapter2" type="checkbox" oninput="checkMark(this)">
-                            <a href="#practice-chapter2">Practice</a>
-                        </li>
-                        <li class="review">
-                            <input id="review-chapter2" type="checkbox" oninput="checkMark(this)">
-                            <a href="#review-chapter2">Review</a>
-                        </li>
-                    </menu>
-                    <h4>Chapter 3: Tree Structures in HTML, CSS and JavaScript</h4>
-                    <menu>
-                        <li class="learn">
-                            <input id="learn-chapter3" type="checkbox" oninput="checkMark(this)">
-                            <a href="#learn-chapter3">Learn</a>
-                        </li>
-                        <li class="practice">
-                            <input id="practice-chapter3" type="checkbox" oninput="checkMark(this)">
-                            <a href="#practice-chapter3">Practice</a>
-                        </li>
-                        <li class="review">
-                            <input id="review-chapter3" type="checkbox" oninput="checkMark(this)">
-                            <a href="#review-chapter3">Review</a>
-                        </li>
-                    </menu>
-                    <h4>Chapter 4: Importing CSS Code</h4>
-                    <menu>
-                        <li class="learn">
-                            <input id="learn-chapter4" type="checkbox" oninput="checkMark(this)">
-                            <a href="#learn-chapter4">Learn</a>
-                        </li>
-                        <li class="practice">
-                            <input id="practice-chapter4" type="checkbox" oninput="checkMark(this)">
-                            <a href="#practice-chapter4">Practice</a>
-                        </li>
-                        <li class="review">
-                            <input id="review-chapter4" type="checkbox" oninput="checkMark(this)">
-                            <a href="#review-chapter4">Review</a>
-                        </li>
-                    </menu>
-                    <h4>Chapter 5: Importing JavaScript Code</h4>
-                    <menu>
-                        <li class="learn">
-                            <input id="learn-chapter5" type="checkbox" oninput="checkMark(this)">
-                            <a href="#learn-chapter5">Learn</a>
-                        </li>
-                        <li class="practice">
-                            <input id="practice-chapter5" type="checkbox" oninput="checkMark(this)">
-                            <a href="#practice-chapter5">Practice</a>
-                        </li>
-                        <li class="review">
-                            <input id="review-chapter5" type="checkbox" oninput="checkMark(this)">
-                            <a href="#review-chapter5">Review</a>
-                        </li>
-                    </menu>
-                </nav>
-            </li>-->
-        </ul>
+        <ul></ul>
         <aside>
             <select onchange="this.getRootNode().host.changeLanguage(event)">
                 <option value="en">English</option>
--- components/sw-main/sw-home/template.mjs
+++ components/sw-main/sw-home/template.mjs
@@ -3,21 +3,31 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="components/sw-main/sw-home/shadow.css">
     <main>
-        <section>
-            <h1>Title</h1>
-        </section>
+        <header>
+            <h1><a href="https://medium.com/siliconwat/a-complete-frontend-developer-textbook-for-beginners-3be48c1f07?sk=351a90e15ab63ee815dda3f3a395f843">A Complete Frontend Developer Course for Beginners<br/>(2023 Edition)</a></h1>
+        </header>
         <section>
             <h2>Learn</h2>
+            <span>📖</span>
+            <ul>
+                <li>You can watch <strong>Lecture Videos</strong> with <em>subtitles</em> at 6 different <em>speeds</em></li>
+                <li>You can read corresponding <strong>Textbook Chapters</strong> with helpful <em>diagrams</em> and <em>side notes</em></li>
+                <li>You can take corresponding <strong>Quizzes</strong> to check your <em>understanding</em></li>
+            </ul>
         </section>
         <section>
             <h2>Practice</h2>
+            <span>💻</span>
         </section>
         <section>
             <h2>Review</h2>
+            <span>👩🏼‍💻</span>
         </section>
-        <section>
-            <h3>Donate</h3>
-        </section>
+        <footer>
+            <p>From here, you can read the entire course textbook on Medium for <strong>free!</strong></p>
+            <p>If you like Medium and use our <a href="https://medium.com/@thonly/membership">referral link</a> to join, we will give you a special coupon to get ALL our <a href="https://siliconwat.com">Udemy courses</a> for <strong>90% OFF</strong> for Life!</p>
+            <p>If you’re already a Medium member or don't like it, please consider donating to <a href="https://github.com/sponsors/SiliconWat">@SiliconWat</a> to receive the same discount!</p>
+        </footer>
     </main>
 `;
 
