c6deb99ae8ed95b4d8b8f425dbca57169cec3a73
Thon Ly <thonly@heartbank.ceo>
2022-11-10 09:39:16 -0800

    begin main layout

--- components/sw-main/sw-home/template.mjs
+++ components/sw-main/sw-home/template.mjs
@@ -3,8 +3,21 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="components/sw-main/sw-home/shadow.css">
     <main>
-        <section>1</section>
-        <section>2</section>
+        <section>
+            <h1>Title</h1>
+        </section>
+        <section>
+            <h2>Learn</h2>
+        </section>
+        <section>
+            <h2>Practice</h2>
+        </section>
+        <section>
+            <h2>Review</h2>
+        </section>
+        <section>
+            <h3>Donate</h3>
+        </section>
     </main>
 `;
 
