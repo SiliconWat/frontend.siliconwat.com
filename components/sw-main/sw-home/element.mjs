23b6ea738d78e914a902c4581a4c1e9c9f66d57b
Thon Ly <thonly@heartbank.ceo>
2022-11-15 16:38:45 -0800

    refactor

--- components/sw-main/sw-home/element.mjs
+++ components/sw-main/sw-home/element.mjs
@@ -19,12 +19,35 @@ class SwHome extends HTMLElement {
         this.shadowRoot.getElementById('flashcard').href = `https://flashcard.siliconwat.com/#${TRILOGY[0].toLowerCase()}`;
         
         this.shadowRoot.querySelectorAll('.medium').forEach(a => a.href = COURSE.medium);
+        this.shadowRoot.querySelectorAll('.program').forEach(span => span.textContent = TRILOGY[1] === 'Course' ? "If" : "When");
+        this.shadowRoot.querySelectorAll('button').forEach(button => button.style.display = TRILOGY[1] === 'Cohort' ? "block" : "none");
+
         this.shadowRoot.querySelectorAll('.cohort').forEach(a => {
             a.textContent = "Remote Cohort Program";
-            a.href = `https://${TRILOGY[0].toLowerCase()}.siliconwat.org`;
+            if (TRILOGY[1] === 'Course') a.href = `https://${TRILOGY[0].toLowerCase()}.siliconwat.org`;
+            if (TRILOGY[1] === 'Cohort') a.style.fontWeight = "bold";
         });
 
+        this.#render();
         this.style.display = 'block';
+    }
+
+    #render() {
+        let project;
+
+        switch (TRILOGY[0]) {
+            case "Frontend":
+                project = "Codepen Project";
+                break;
+            case "Backend":
+                project = "Replit Project";
+                break;
+            case "iOS":
+                project = "XCode Project";
+                break;
+        }
+
+        this.shadowRoot.getElementById('project').textContent = project;
     }
 }
 
--- components/sw-main/sw-home/template.mjs
+++ components/sw-main/sw-home/template.mjs
@@ -14,26 +14,32 @@ template.innerHTML = `
                 <li>If you purchase my <a id="udemy">Udemy Course</a>, you can watch my <strong>Lecture Videos</strong> with <em>subtitles</em> at 7 different <em>speeds</em> (from &half; to 2x).</li>
                 <li>You can read my corresponding <a class="medium">Textbook Chapters</a> with <em>helpful diagrams</em> and <strong>side notes</strong> on Medium for FREE.</li>
                 <li>You can take my corresponding <a id="quiz">Quizzes</a> for FREE to check your <em>understanding</em> and earn <strong>SW Coins</strong>!</li>
-                <li>If you join my <a class="cohort"></a>, you can form <strong>Study Groups</strong> with your <em>classmates</em> on Discord.</li>
+                <li><span class="program"></span> you join my <a class="cohort"></a>, you can form <strong>Study Groups</strong> with your <em>classmates</em> on Discord.</li>
             </ul>
+            <br>
+            <button><span>Join Cohort</span></button>
         </section>
         <section>
             <h2>Practice</h2>
             <figure>💻</figure>
             <ul>
                 <li>You can play my <em>interactive</em> <a id="code">Code Exercises</a> for FREE to test your <em>coding skills</em> and earn <strong>SW Coins</strong>!</li>
-                <li>If you join my <a class="cohort"></a>, you can <strong>Pair Program</strong> with your <em>classmates</em> on Discord.</li>
-                <li>If you join my <a class="cohort"></a>, you can submit your <strong>Codepen Project</strong> for <em>grading</em> and <em>ranking</em> by your classmates.</li>
+                <li><span class="program"></span> you join my <a class="cohort"></a>, you can <strong>Pair Program</strong> with your <em>classmates</em> on Discord.</li>
+                <li><span class="program"></span> you join my <a class="cohort"></a>, you can submit your <strong id="project"></strong> for <em>grading</em> and <em>ranking</em> by your classmates.</li>
             </ul>
+            <br>
+            <button><span>Sign In</span></button>
         </section>
         <section>
             <h2>Review</h2>
             <figure>👩🏼‍💻</figure>
             <ul>
                 <li>You can play my <a id="flashcard">Flashcard Games</a> for FREE to remember <em>code syntaxes</em> and earn <strong>SW Coins</strong>!</li>
-                <li>If you join my <a class="cohort"></a>, you can submit your <strong>Medium Summaries</strong> for <em>grading</em> and <em>comments</em> by your classmates.</li>
+                <li><span class="program"></span> you join my <a class="cohort"></a>, you can submit your <strong>Medium Summaries</strong> for <em>grading</em> and <em>comments</em> by your classmates.</li>
                 <li>After you successfully finish my <a class="cohort"></a>, you can sign up for a <strong>Mock Coding Interview</strong> with me about <em>Data Structures</em> and <em>Algorithms</em> on Discord.</li>
             </ul>
+            <br>
+            <button><span>Private Discord</span></button>
         </section>
         <footer></footer>
     </main>
