33244d1afad4eaef9cb63af957cb4707b7e37fa9
Thon Ly <thonly@heartbank.ceo>
2023-01-02 11:33:34 -0800

    refactor

--- components/sw-footer/template.mjs
+++ components/sw-footer/template.mjs
@@ -29,7 +29,7 @@ template.innerHTML = `
                 <div>
                     <h5>Live Demos</h5>
                     <ul id="frontend">
-                        <li><a href="https://music.siliconwat.com">Music Editor</a></li>
+                        <li><a href="https://music.siliconwat.com">Music PWA</a></li>
                         <li><a href="https://music.siliconwat.org">Music School</a></li>
                     </ul>
                     <ul id="backend">
